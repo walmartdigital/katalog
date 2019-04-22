1e9c337dce7d33e84b5eee8d5d214f5c936ad36d
Max Guzman <max.guzman@icloud.com>
2019-04-22 07:35:14 -0400

    test(server): wip testing.

--- src/server/server_test.go
+++ src/server/server_test.go
@@ -1,65 +1,67 @@
 package server_test
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/emirpasic/gods/lists/arraylist"
 	"github.com/gorilla/mux"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/walmartdigital/katalog/src/domain"
 	"github.com/walmartdigital/katalog/src/server"
-	"github.com/walmartdigital/katalog/src/utils"
 )
 
-type dummyRepository struct{}
+type dummyRepository struct {
+	services []domain.Service
+}
 
 func (r *dummyRepository) CreateService(obj interface{}) {}
 func (r *dummyRepository) DeleteService(obj interface{}) {}
 func (r *dummyRepository) GetAllServices() []interface{} {
-	return nil
+	list := arraylist.New()
+	return list.Values()
 }
 
 type dummyRouter struct{}
 
 func (r *dummyRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
 	fmt.Println("***")
 }
 
-var router = mux.NewRouter().StrictSlash(true)
+// var router = mux.NewRouter().StrictSlash(true)
 var routes = make(map[string]func(http.ResponseWriter, *http.Request))
 
 func (r *dummyRouter) HandleFunc(path string, f func(http.ResponseWriter, *http.Request)) *mux.Route {
 	fmt.Println(path)
 	routes[path] = f
-	return nil
+	route := &mux.Route{}
+	return route
 }
 
 func TestAll(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "server")
+	RunSpecs(t, "Server")
 }
 
 var _ = Describe("run server", func() {
-	It("should receive all services", func() {
-		path := "/services"
-		req, _ := http.NewRequest("GET", "localhost:10000"+path, nil)
-		recorder := httptest.NewRecorder()
+	It("should receive status code 200", func() {
 		repository := &dummyRepository{}
 		router := &dummyRouter{}
 		server := server.CreateServer(repository, router)
-		routes[path](recorder, req)
-		res := recorder.Result()
-		defer res.Body.Close()
-		b, _ := ioutil.ReadAll(res.Body)
-		service := string(bytes.TrimSpace(b))
-
 		server.Run()
+		path := "/services"
+		req, _ := http.NewRequest(http.MethodGet, "http://127.0.0.1:10000"+path, nil)
+		rec := httptest.NewRecorder()
 
-		Expect(utils.Deserialize(service)).To(Equal())
+		// routes[path](rec, req)
+		router.ServeHTTP(rec, req)
 
+		res := rec.Result()
+		defer res.Body.Close()
+		Expect(res.StatusCode).To(Equal(http.StatusOK))
 	})
 })
