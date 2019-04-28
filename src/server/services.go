f5f5eca61fd890fae931c0712cd9078ef20c92d5
Max Guzman <max.guzman@icloud.com>
2019-04-28 14:23:21 -0400

    fix(server): delete service using service id.

--- src/server/services.go
+++ src/server/services.go
@@ -2,8 +2,10 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/walmartdigital/katalog/src/domain"
 )
 
@@ -15,10 +17,9 @@ func (s *Server) createService(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) deleteService(w http.ResponseWriter, r *http.Request) {
-	var service domain.Service
-	json.NewDecoder(r.Body).Decode(&service)
-	s.serviceRepository.DeleteService(service)
-	json.NewEncoder(w).Encode(service)
+	id := mux.Vars(r)["id"]
+	s.serviceRepository.DeleteService(id)
+	fmt.Fprintf(w, "deleted service id: %s", id)
 }
 
 func (s *Server) getAllServices(w http.ResponseWriter, r *http.Request) {
