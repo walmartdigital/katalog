66f6755027ab70fb0eff3803e6f4b22e2bea2402
Sebastian Diaz <sebastian.diaz@walmart.com>
2019-06-11 17:17:01 -0400

    test(k8s-driver): add test file

---
+++ src/collector/k8s-driver/k8s-driver_test.go
@@ -0,0 +1,26 @@
+package k8sdriver_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	k8sdriver "github.com/walmartdigital/katalog/src/collector/k8s-driver"
+)
+
+func TestAll(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "memory persistence")
+}
+
+var _ = Describe("build driver", func() {
+	It("should build a driver", func() {
+		kubePath := ""
+		excludeSystemNamespace := true
+
+		output := k8sdriver.BuildDriver(kubePath, excludeSystemNamespace)
+
+		expect(output)
+	})
+
+})
