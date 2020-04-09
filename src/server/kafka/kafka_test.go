b46fa7fb7c5313c4cd090c4f6422f75942697aff
Chin Dou <chin.dou@walmart.com>
2020-04-09 09:19:41 -0400

    test(kafka/consumer): complete skaffolding for testing consumer with all mocked dependencies (#2) [CI SKIP]

--- src/server/kafka/kafka_test.go
+++ src/server/kafka/kafka_test.go
@@ -24,37 +24,44 @@ func TestAll(t *testing.T) {
 }
 
 var _ = Describe("run server", func() {
-	var ()
+	var (
+		fakeReaderFactory  *mock_kafka.MockReaderFactory
+		fakeReader         *mock_kafka.MockReader
+		fakeRepoFactory    *mock_repositories.MockRepositoryFactory
+		fakeRepo           *mock_repositories.MockRepository
+		fakeMetricsFactory *mock_server.MockMetricsFactory
+		fakeMetrics        *mock_server.MockMetrics
+		consumer           *kafka.Consumer
+	)
 
 	BeforeEach(func() {
 		// Initialize the mocked Kafka related objects
-		fakeReaderFactory := mock_kafka.NewMockReaderFactory(ctrl)
-		fakeReader := mock_kafka.NewMockReader(ctrl)
+		fakeReaderFactory = mock_kafka.NewMockReaderFactory(ctrl)
+		fakeReader = mock_kafka.NewMockReader(ctrl)
 		fakeReaderFactory.EXPECT().Create(gomock.Any(), gomock.Any()).Return(
 			fakeReader,
 		).Times(3)
 
 		// Initialize the mocked Repository related objects
-		fakeRepoFactory := mock_repositories.NewMockRepositoryFactory(ctrl)
-		fakeRepo := mock_repositories.NewMockRepository(ctrl)
+		fakeRepoFactory = mock_repositories.NewMockRepositoryFactory(ctrl)
+		fakeRepo = mock_repositories.NewMockRepository(ctrl)
 		fakeRepoFactory.EXPECT().Create().Return(
 			fakeRepo,
 		).Times(1)
 
 		// Initialize the mocked Metrics related objects
-		fakeMetricsFactory := mock_server.NewMockMetricsFactory(ctrl)
-		fakeMetrics := mock_server.NewMockMetrics(ctrl)
+		fakeMetricsFactory = mock_server.NewMockMetricsFactory(ctrl)
+		fakeMetrics = mock_server.NewMockMetrics(ctrl)
 		fakeMetrics.EXPECT().IncrementCounter(gomock.Any(), gomock.Any()).AnyTimes()
 		fakeMetricsFactory.EXPECT().Create().Return(
 			fakeMetrics,
 		).Times(1)
 
-		consumer := kafka.CreateConsumer("", "", fakeReaderFactory, fakeRepoFactory, fakeMetricsFactory)
-		_ = consumer
+		consumer = kafka.CreateConsumer("", "", fakeReaderFactory, fakeRepoFactory, fakeMetricsFactory)
 	})
 
-	It("should create a service", func() {
-		Expect("hello").To(Equal("hello"))
+	It("should create a consumer", func() {
+		Expect(consumer).NotTo(BeNil())
 	})
 
 	AfterEach(func() {
