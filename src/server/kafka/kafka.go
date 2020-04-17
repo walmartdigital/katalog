eac6482a0cbe5abe4d055ccb789e206ec910c5b0
Chin Dou <chin.dou@walmart.com>
2020-04-17 12:18:08 -0400

    refactor(kafka): use a Context to be able to receive cancellation signals from parent thread [CI SKIP]

--- src/server/kafka/kafka.go
+++ src/server/kafka/kafka.go
@@ -45,10 +45,13 @@ type Consumer struct {
 	KafkaReaders        map[string]*Reader
 	resourcesRepository repositories.Repository
 	service             server.Service
+	context             context.Context
+	mux                 sync.Mutex
+	activeTopics        map[string]string
 }
 
 // CreateConsumer ...
-func CreateConsumer(kafkaURL string, topicPrefix string, readerFactory ReaderFactory, repoFactory repositories.RepositoryFactory, metricsFactory server.MetricsFactory) *Consumer {
+func CreateConsumer(ctx context.Context, kafkaURL string, topicPrefix string, readerFactory ReaderFactory, repoFactory repositories.RepositoryFactory, metricsFactory server.MetricsFactory) *Consumer {
 	created := readerFactory.Create(kafkaURL, topicPrefix+".created")
 	deleted := readerFactory.Create(kafkaURL, topicPrefix+".deleted")
 	updated := readerFactory.Create(kafkaURL, topicPrefix+".updated")
@@ -62,10 +65,11 @@ func CreateConsumer(kafkaURL string, topicPrefix string, readerFactory ReaderFac
 			"deleted": &deleted,
 			"updated": &updated,
 		},
+		context:      ctx,
+		activeTopics: make(map[string]string),
 	}
 
 	current.service = server.MakeService(current.resourcesRepository, metricsFactory)
-
 	return current
 }
 
@@ -80,96 +84,118 @@ func (c *Consumer) Run() {
 	wg.Wait()
 }
 
+// IsTopicActive ...
+func (c *Consumer) IsTopicActive(event string) bool {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	t := c.activeTopics[event]
+	if t == "active" {
+		return true
+	}
+	return false
+}
+
 // ConsumeEvent ...
 func (c *Consumer) ConsumeEvent(event string) {
 	defer wg.Done()
-
+	c.mux.Lock()
+	c.activeTopics[event] = "active"
+	c.mux.Unlock()
 	consumer := c.KafkaReaders[event]
 
 	defer (*consumer).Close()
 
 	for {
-		m, err := (*consumer).ReadMessage(context.Background())
-		if err != nil {
-			break
-		}
-
-		key := string(m.Key)
-		value := string(m.Value)
-
-		log.WithFields(logrus.Fields{
-			"key":    key,
-			"offset": m.Offset,
-		}).Debug("Message Received")
-
-		matchedNamedGroups := regex.GetParams(
-			"/(?P<artifact>.+)/(?P<id>.+)",
-			key,
-		)
-
-		artifact := matchedNamedGroups["artifact"]
-		id := matchedNamedGroups["id"]
-
-		log.WithFields(logrus.Fields{
-			"event":    event,
-			"artifact": artifact,
-			"id":       id,
-		}).Debug("Event processing")
-
-		switch event {
-		case "created":
-			switch artifact {
-			case "services":
-				go c.CreateService(value)
-			case "deployments":
-				go c.CreateDeployment(value)
-			case "statefulsets":
-				go c.CreateStatefulSet(value)
-			default:
-				log.WithFields(logrus.Fields{
-					"event":    event,
-					"artifact": artifact,
-				}).Warn("Artifact not recognized")
-			}
-		case "updated":
-			switch artifact {
-			case "services":
-				go c.UpdateService(value)
-			case "deployments":
-				go c.UpdateDeployment(value)
-			case "statefulsets":
-				go c.UpdateStatefulSet(value)
-			default:
-				log.WithFields(logrus.Fields{
-					"event":    event,
-					"artifact": artifact,
-				}).Warn("Artifact not recognized")
-			}
-		case "deleted":
-			switch artifact {
-			case "services":
-				go c.DeleteService(id)
-			case "deployments":
-				go c.DeleteDeployment(id)
-			case "statefulsets":
-				go c.DeleteStatefulSet(id)
-			default:
-				log.WithFields(logrus.Fields{
-					"event":    event,
-					"artifact": artifact,
-				}).Warn("Artifact not recognized")
-			}
+		select {
+		case <-c.context.Done():
+			log.Info("Received cancel signal from parent context")
+			c.mux.Lock()
+			c.activeTopics[event] = ""
+			c.mux.Unlock()
+			return
 		default:
+			m, err := (*consumer).ReadMessage(c.context)
+			if err != nil {
+				break
+			}
+
+			key := string(m.Key)
+			value := string(m.Value)
+
+			log.WithFields(logrus.Fields{
+				"key":    key,
+				"offset": m.Offset,
+			}).Debug("Message Received")
+
+			matchedNamedGroups := regex.GetParams(
+				"/(?P<artifact>.+)/(?P<id>.+)",
+				key,
+			)
+
+			artifact := matchedNamedGroups["artifact"]
+			id := matchedNamedGroups["id"]
+
 			log.WithFields(logrus.Fields{
 				"event":    event,
 				"artifact": artifact,
-			}).Warn("Event not recognized")
-		}
+				"id":       id,
+			}).Debug("Event processing")
 
-		log.WithFields(logrus.Fields{
-			"event":    event,
-			"artifact": artifact,
-			"id":       id,
-		}).Debug("Event process task launched")
+			switch event {
+			case "created":
+				switch artifact {
+				case "services":
+					go c.CreateService(value)
+				case "deployments":
+					go c.CreateDeployment(value)
+				case "statefulsets":
+					go c.CreateStatefulSet(value)
+				default:
+					log.WithFields(logrus.Fields{
+						"event":    event,
+						"artifact": artifact,
+					}).Warn("Artifact not recognized")
+				}
+			case "updated":
+				switch artifact {
+				case "services":
+					go c.UpdateService(value)
+				case "deployments":
+					go c.UpdateDeployment(value)
+				case "statefulsets":
+					go c.UpdateStatefulSet(value)
+				default:
+					log.WithFields(logrus.Fields{
+						"event":    event,
+						"artifact": artifact,
+					}).Warn("Artifact not recognized")
+				}
+			case "deleted":
+				switch artifact {
+				case "services":
+					go c.DeleteService(id)
+				case "deployments":
+					go c.DeleteDeployment(id)
+				case "statefulsets":
+					go c.DeleteStatefulSet(id)
+				default:
+					log.WithFields(logrus.Fields{
+						"event":    event,
+						"artifact": artifact,
+					}).Warn("Artifact not recognized")
+				}
+			default:
+				log.WithFields(logrus.Fields{
+					"event":    event,
+					"artifact": artifact,
+				}).Warn("Event not recognized")
+			}
+
+			log.WithFields(logrus.Fields{
+				"event":    event,
+				"artifact": artifact,
+				"id":       id,
+			}).Debug("Event process task launched")
+		}
 	}
 }
