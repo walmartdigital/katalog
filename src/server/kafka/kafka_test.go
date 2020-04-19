5ac5aea8c6d1dc3016e31aff7b8e01c008c82feb
chinniehendrix <chinniehendrix@gmail.com>
2020-04-18 20:00:10 -0400

    test(kafka): add test that successfully updates a deployment [CI SKIP]

--- src/server/kafka/kafka_test.go
+++ src/server/kafka/kafka_test.go
@@ -29,7 +29,7 @@ func TestAll(t *testing.T) {
 	RunSpecs(t, "Server")
 }
 
-var _ = Describe("run consumer", func() {
+var _ = Describe("Run Consumer on 'created' topic", func() {
 	var (
 		fakeReaderFactory  *mock_kafka.MockReaderFactory
 		fakeReader         *mock_kafka.MockReader
@@ -226,6 +226,108 @@ var _ = Describe("run consumer", func() {
 		)
 		Expect(consumer).NotTo(BeNil())
 		go consumer.Run()
+	})
+
+	AfterEach(func() {
+	})
+})
+
+var _ = Describe("Run Consumer on 'updated' topic", func() {
+	var (
+		fakeReaderFactory  *mock_kafka.MockReaderFactory
+		fakeReader         *mock_kafka.MockReader
+		fakeRepoFactory    *mock_repositories.MockRepositoryFactory
+		fakeRepo           *mock_repositories.MockRepository
+		fakeMetricsFactory *mock_server.MockMetricsFactory
+		fakeMetrics        *mock_server.MockMetrics
+		upconsumer         *kafka.Consumer
+		ctx                context.Context
+		cancel             context.CancelFunc
+		wg                 *sync.WaitGroup
+		service            server.Service
+	)
+
+	BeforeEach(func() {
+		// Initialize the mocked Kafka related objects
+		fakeReaderFactory = mock_kafka.NewMockReaderFactory(ctrl)
+		fakeReader = mock_kafka.NewMockReader(ctrl)
+		fakeReaderFactory.EXPECT().Create(gomock.Any(), gomock.Any()).Return(
+			fakeReader,
+		).Times(1)
+
+		// Initialize the mocked Repository related objects
+		fakeRepoFactory = mock_repositories.NewMockRepositoryFactory(ctrl)
+		fakeRepo = mock_repositories.NewMockRepository(ctrl)
+		fakeRepoFactory.EXPECT().Create().Return(
+			fakeRepo,
+		).Times(1)
+
+		// Initialize the mocked Metrics related objects
+		fakeMetricsFactory = mock_server.NewMockMetricsFactory(ctrl)
+		fakeMetrics = mock_server.NewMockMetrics(ctrl)
+		fakeMetrics.EXPECT().IncrementCounter(gomock.Any(), gomock.Any()).AnyTimes()
+		fakeMetricsFactory.EXPECT().Create().Return(
+			fakeMetrics,
+		).Times(1)
+
+		ctx, cancel = context.WithCancel(context.Background())
+		service = server.MakeService(fakeRepoFactory.Create(), fakeMetricsFactory)
+		wg = new(sync.WaitGroup)
+		upconsumer = kafka.CreateConsumer(ctx, wg, "", "", "updated", fakeReaderFactory, &service)
+	})
+
+	It("should create a consumer", func() {
+		Expect(upconsumer).NotTo(BeNil())
+	})
+
+	It("should update a Deployment", func() {
+		var testwg sync.WaitGroup
+		testwg.Add(1)
+		defer testwg.Wait()
+		ss := domain.Deployment{
+			ID:         "276797fa-b207-11e9-8527-000d3af9d6b6",
+			Name:       "queue-node",
+			Generation: 8,
+			Namespace:  "amida",
+			Labels: map[string]string{
+				"HEAD":                   "569de2ecd9f9357b3380664f43c90d07ec6acaff",
+				"app":                    "nats",
+				"fluxcd.io/sync-gc-mark": "sha256.0fRlq9kqkh2eSDRqXANMzgN8_8jeguja3eDLoE5E0Xo",
+				"some":                   "change",
+			},
+			Containers: map[string]string{
+				"nats-exporter":  "synadia/prometheus-nats-exporter:0.4.0",
+				"nats-streaming": "nats-streaming:0.15.1",
+			},
+		}
+
+		ssbytes, _ := json.Marshal(ss)
+
+		message := kafgo.Message{
+			Topic:     "_katalog.artifact.updated",
+			Partition: 1,
+			Offset:    5,
+			Key:       []byte("/deployments/276797fa-b207-11e9-8527-000d3af9d6b6"),
+			Value:     ssbytes,
+			Headers:   nil,
+			Time:      time.Now(),
+		}
+
+		resource := domain.Resource{K8sResource: &ss}
+		fakeReader.EXPECT().Close().Times(1)
+		fakeRepo.EXPECT().UpdateResource(resource).Return(&resource, nil).Times(1).Do(
+			func(r domain.Resource) {
+				testwg.Done()
+			},
+		)
+		fakeReader.EXPECT().ReadMessage(ctx).Return(message, nil).Times(1).Do(
+			func(c context.Context) {
+				cancel()
+			},
+		)
+		wg.Add(1)
+		go upconsumer.Run()
+		wg.Wait()
 	})
 
 	AfterEach(func() {
