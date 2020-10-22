c525bacf56a169e2d49ce34e957665db6a63a442
piperfe <felipe.alfaro@walmart.com>
2020-10-22 09:57:25 -0300

    BUSHIDO-28 (REFACTOR-TEST): encapsulate the deployment struct creation in a method / fix lint in export method

--- collector/k8s-driver/deployment-factory_test.go
+++ collector/k8s-driver/deployment-factory_test.go
@@ -21,40 +21,7 @@ var _ = Describe("Deployment builder struct", func() {
 	BeforeEach(func() {})
 
 	It("should build a deployment when pass k8Deployment", func() {
-
-		sourceDeployment := &appsv1.Deployment{
-			TypeMeta: metav1.TypeMeta{},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:        "NameExample",
-				Namespace:   "NameSpaceExample",
-				UID:         "UIDExample",
-				Generation:  5,
-				Labels:      map[string]string{"keyLabelExample": "valueLabelExample"},
-				Annotations: map[string]string{"keyAnnotationsExample": "valueAnnotationsExample"},
-			},
-			Spec: appsv1.DeploymentSpec{
-				Replicas: nil,
-				Selector: nil,
-				Template: corev1.PodTemplateSpec{
-					ObjectMeta: metav1.ObjectMeta{},
-					Spec: corev1.PodSpec{
-						Containers: []corev1.Container{{
-							Name:  "containerNameExample",
-							Image: "containerImageExample",
-						}},
-					},
-				},
-				Strategy:                appsv1.DeploymentStrategy{},
-				MinReadySeconds:         0,
-				RevisionHistoryLimit:    nil,
-				Paused:                  false,
-				ProgressDeadlineSeconds: nil,
-			},
-			Status: appsv1.DeploymentStatus{
-				ObservedGeneration: 1,
-			},
-		}
-		deployment := BuildDeploymentFromK8sDeployment(sourceDeployment)
+		deployment := BuildDeploymentFromK8sDeployment(buildDeployment())
 
 		Expect(deployment.GetID()).To(Equal("UIDExample"))
 		Expect(deployment.GetObservedGeneration()).To(Equal(int64(1)))
@@ -68,3 +35,31 @@ var _ = Describe("Deployment builder struct", func() {
 	})
 
 })
+
+func buildDeployment() *appsv1.Deployment {
+	return &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "NameExample",
+			Namespace:   "NameSpaceExample",
+			UID:         "UIDExample",
+			Generation:  5,
+			Labels:      map[string]string{"keyLabelExample": "valueLabelExample"},
+			Annotations: map[string]string{"keyAnnotationsExample": "valueAnnotationsExample"},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Name:  "containerNameExample",
+						Image: "containerImageExample",
+					}},
+				},
+			},
+		},
+		Status: appsv1.DeploymentStatus{
+			ObservedGeneration: 1,
+		},
+	}
+}
